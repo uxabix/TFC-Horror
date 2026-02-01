f1ffd8c148ef718e8d6fb19659af3f6c920d2c95
Kiryl Alishkevich <kirill.olishkevitch@gmail.com>
2026-02-01 19:41:17 +0100

    Better Class structure, Minor Cooking System Improvements Simple dbs for Food and CookingSurface, Simple FoodManager, CookingSurfaceManager

---
+++ src/shared/Data/FoodData.lua
@@ -0,0 +1,19 @@
+local FoodDatabase = {
+	Pizza = {
+		CookTime = 5,
+		BurnTime = 3,
+		SurfaceType = "Stove", -- CookingSurfaceType
+	},
+	Steak = {
+		CookTime = 7,
+		BurnTime = 4,
+		SurfaceType = "Grill",
+	},
+	Cake = {
+		CookTime = 10,
+		BurnTime = 5,
+		SurfaceType = "Oven",
+	},
+}
+
+return FoodDatabase
